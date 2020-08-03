dd8ba412e1d853ffb53636e8c80dada10189a376
Set <6942774+setzerbomb@users.noreply.github.com>
2020-08-03 19:56:25 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,12 @@
+/**
+ * Babel: Converter (transpilar) códico do React para um código entendível pelo browser
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) irá ser usada uma forma diferente de conversão
+ *
+ * Loaders: babel-loader, css-loader, image-loader
+ *
+ * yarn add @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli
+ *
+ * yarn babel src/index.js  --out-file public/bundle.js
+ *
+ * yarn webpack-dev-server --mode development
+ */
